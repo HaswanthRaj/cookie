6c376ae7d9fbf8e918ba82daacc1a9afc7c76315
haswanth <haswanthraj777@gmail.com>
2021-01-06 10:58:07 +0530

    newo

--- commands/Economy/add.js
+++ commands/Economy/add.js
@@ -5,9 +5,7 @@ module.exports={
     name: "add",
     category: "Economy",
     run: async(client, message, args)=>{
-        if(!message.member.hasPermissions('ADMINISTRATOR')){
-            return message.channel.send('You can\'t use this command')
-        }
+        if (!message.member.hasPermission("ADMINISTRATOR") || !message.guild.owner) return message.channel.send("You don't have enough authority to do this.");
         if(!args[0]){
             message.channel.send('Mention a user to send money :)');
         }
@@ -18,7 +16,7 @@ module.exports={
           return message.reply('Type `\'add <mention> <cash>` to add cash to others')
 
         }
-        await add(user, message.author.tag, args[1])
+        await add(user, user.user.tag, args[1])
         message.channel.send(`successfully added ${args[1]} to <@${user}>`)
     }
 }
