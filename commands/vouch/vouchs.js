8f610e662cb121d9d384ba66b6838cab4c61a90e
haswanth <haswanthprime123@gmail.com>
2021-01-17 22:14:58 +0530

    ne4

--- commands/vouch/vouchs.js
+++ commands/vouch/vouchs.js
@@ -13,19 +13,58 @@ module.exports={
         let data = await vouch(user, message)
 
         let voch = data.vouch
-       /* let msg
-        if(voch < 10){
-             msg = "**This  user not Trusted Yet vouchs:**"
+        let opt
+        if(voch < 1){
+             opt = 1
         }
         if(voch >= 10){
-            msg= " **This user Trusted worthy vouchs: **"
+            opt = 2
         }
         if(voch > 20){
-            msg= "**This user Trusted vouchs: **"
+          
+            opt= 3
         }
         if(voch >= 30){
-            msg= "**This user Super Trusted vouchs: **"
-        }*/
-        message.channel.send(`**Vouches: ${voch}**`)
+            opt = 4
+        }
+
+        switch(opt){
+            case 1:{
+                const emb = new Discord.MessageEmbed()
+                .setColor('FF0000')
+                .setTitle('Vouches')
+                .setDescription(`**This  user not Trusted Yet vouchs: ${voch}**`)
+                message.channel.send(emb)
+            }
+            break;
+            case 2:{
+                const emb = new Discord.MessageEmbed()
+                .setColor('E38A00')
+                .setTitle('Vouches')
+                .setDescription(`**This  user Trust worthy vouchs: ${voch}**`)
+                message.channel.send(emb)
+            }
+            break;
+            case 3:{
+                const emb = new Discord.MessageEmbed()
+                .setColor('68FC01')
+                .setTitle('Vouches')
+                .setDescription(`**This  user Trusted vouchs: ${voch}**`)
+                message.channel.send(emb)
+            }
+            break;
+            case 4:{
+                const emb = new Discord.MessageEmbed()
+                .setColor('01FC09')
+                .setTitle('Vouches')
+                .setDescription(`**This  user Super Trusted vouchs: ${voch}**`)
+                message.channel.send(emb)
+            }
+            break;
+            default:{
+                return
+            }
+
+        }
     }//ok
 }//add
