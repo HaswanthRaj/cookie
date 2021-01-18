d953cbbb1e66c7610535e97e249f494e4c8476ab
haswanth <haswanthprime123@gmail.com>
2021-01-18 07:49:00 +0530

    new ds

--- commands/vouch/vouch.js
+++ commands/vouch/vouch.js
@@ -7,7 +7,10 @@ module.exports={
     category: "Economy",
     run: async(client, message, args)=>{
         if(!message.channel == "800379396808376340") return
-        if (member.id === message.author.id){
+        var user = message.mentions.users.first().id 
+        
+        if(!user) return message.member.send('Type`\' vouch <mention>` to add cash to others')
+        if (user === message.author.id){
             const red = new Discord.MessageEmbed()
             red.setDescription(`**You can't Vouch yourself**`)
             return message.channel.send(red); 
@@ -16,9 +19,7 @@ module.exports={
         if(!args[0]){
             message.channel.send('Mention a user to send vouch :)');
         }
-            var user = message.mentions.users.first().id 
-        
-            if(!user) return message.member.send('Type`\' vouch <mention>` to add cash to others')
+           
         await addvouch(user)
         message.react("✅")
     }//ok
